fdf2b774193422db4da360f9d29334bc399a352a
EL-MANANIFatima <fatimaelmanani80@gmail.com>
2023-12-19 21:48:58 +0100

    Initial commit

---
+++ src/test/java/com/example/summerschool/SummerSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.summerschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
